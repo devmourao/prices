5f3fc51dd1933bfdd45ee32e07df81733d91d342
Marcos Mourão <dev@mourao.info>
2026-09-09 00:20:34 -0300

    feat(CB-05): add i18n skeleton with en default and pt-BR resources

---
+++ src/i18n/index.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en.json'
+import ptBR from './locales/pt-BR.json'
+
+void i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      'pt-BR': { translation: ptBR },
+    },
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'pt-BR'],
+    detection: {
+      order: ['localStorage', 'navigator'],
+      lookupLocalStorage: 'prices-locale',
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+
+export default i18n
